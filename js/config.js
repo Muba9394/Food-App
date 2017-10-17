b34a11530900c21b06481035ee10943aa62b2839
MubarakKhan <Muba9394@users.noreply.github.com>
2017-10-17 10:13:44 +0530

    This is the master

---
+++ js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://www.cuisine.je/mobileapp/api",
+	'DialogDefaultTitle' : "Take Away",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
+	'APIHasKey' : "db03847be0c7a3fb303f70f30520225c"
+};
